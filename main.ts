43d4d670003858614fd3ae2bb3f526b286aa927d
Farsley <76780317+Farsley@users.noreply.github.com>
2020-12-31 19:57:08 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+if (true) {
+    basic.showIcon(IconNames.Heart)
+}
+while (false) {
+    music.playMelody("- - E A E G - - ", 120)
+}
+pins.setAudioPin(AnalogPin.P0)
+basic.forever(function () {
+	
+})
